104a4022d730ffc6f0aced167b75c75f0aa5dfb0
KevinLJW <evin92@gmail.com>
2015-05-03 18:11:09 +0800

    svm ex

--- phrase1/libsvm-3.20/python/svmutil.py
+++ phrase1/libsvm-3.20/python/svmutil.py
@@ -33,6 +33,26 @@ def svm_read_problem(data_file_name):
 		prob_x += [xi]
 	return (prob_y, prob_x)
 
+def svm_read_problemEX(data_file_name):
+	"""
+	svm_read_problem(data_file_name) -> [y, x]
+
+	Read LIBSVM-format data from data_file_name and return labels y
+	and data instances x.
+	"""
+	prob_x = []
+	for line in open(data_file_name):
+		line = line.split(None, 1)
+		# In case an instance with all zero features
+		if len(line) == 1: line += ['']
+		label, features = line
+		xi = {}
+		for e in features.split():
+			ind, val = e.split(":")
+			xi[int(ind)] = float(val)
+		prob_x += [xi]
+	return (prob_x)
+
 def svm_load_model(model_file_name):
 	"""
 	svm_load_model(model_file_name) -> model

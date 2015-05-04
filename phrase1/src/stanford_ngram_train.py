f9d8ca5aee96998d03279b9403c96263dc3a9b8f
pp31630 <pp31630@gmail.com>
2015-05-05 00:07:31 +0800

    Update stanford_ngram_train.py

--- phrase1/src/stanford_ngram_train.py
+++ phrase1/src/stanford_ngram_train.py
@@ -2,13 +2,15 @@
 
 import numpy as np
 
-file_td = file('tmp/mixParsed.txt', 'r')    # training data
-file_rn = file('tmp/rightNgram.txt', 'r')   # right bigram
-file_wn = file('tmp/wrongNgram.txt', 'r')   # wrong bigram
-file_pb = file('tmp/train_prob.txt', 'w')   # output probability
+file_td = file('mixParsed.txt', 'r')    # training data
+file_rn = file('rightNgram.txt', 'r')   # right bigram
+file_wn = file('wrongNgram.txt', 'r')   # wrong bigram
+file_pb = file('train_avg_prob.txt', 'w')   # output probability
 
 NgramTableR = {}
 NgramTableW = {}
+NgramKeyR = []
+NgramKeyW = []
 countListR = []
 countListW = []
 KatzTableR = {}
@@ -27,22 +29,28 @@ def katz(c, table):
     return cs
 
 for line in file_rn:
+    line = line.decode('utf8')
     line = line.split(' ')
     word = line[0]
     count = int(line[1])
     total_right_ngram += count
     NgramTableR[word] = count
+    if word not in NgramKeyR:
+        NgramKeyR.append(word)
     if count not in countListR:
         countListR.append(count)
         KatzTableR[count] = 0
     KatzTableR[count] += 1
 
 for line in file_wn:
+    line = line.decode('utf8')
     line = line.split(' ')
     word = line[0]
     count = int(line[1])
     total_wrong_ngram += count
     NgramTableW[word] = count
+    if word not in NgramKeyW:
+        NgramKeyW.append(word)
     if count not in countListW:
         countListW.append(count)
         KatzTableW[count] = 0
@@ -59,13 +67,13 @@ for line in file_td:
     tokens = sline.split(' ')
     r_prob = 1
     for token in tokens:
-        if token in NgramTableR:
+        if token in NgramKeyR:
             r_prob *= katz(NgramTableR[token], KatzTableR) / total_right_ngram
         else:
             r_prob *= katz(0, KatzTableR) / total_right_ngram
     w_prob = 1
     for token in tokens:
-        if token in NgramTableW:
+        if token in NgramKeyW:
             w_prob *= katz(NgramTableW[token], KatzTableW) / total_wrong_ngram
         else:
             w_prob *= katz(0, KatzTableW) / total_wrong_ngram

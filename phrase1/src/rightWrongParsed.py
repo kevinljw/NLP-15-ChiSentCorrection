02d469dfa059959ba2ef3ef1c172b6c13ff73b8b
brian7685 <mac@macdeMacBook-Air-3.local>
2015-05-04 19:48:18 +0800

    rightWrongParsed

---
+++ phrase1/src/rightWrongParsed.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+nGram = {}
+nGramHashKeyList = []
+line = open("../input/240365_p1.train.txt",'r')
+parsed = open("../tmp/segResult.txt",'r')
+rightNgram = open("../tmp/rightParsed.txt",'w')
+worngNgram = open("../tmp/wrongParsed.txt",'w')
+
+for i in xrange(5924):#5924 2962
+    data = line.readline()
+    sentence = parsed.readline()
+    token = data.split()
+
+    if int(token[1]) is 0:
+        rightNgram.write(str(token[0])+'\t'+str(token[1])+'\t'+sentence)
+    else:
+        worngNgram.write(str(token[0])+'\t'+str(token[1])+'\t'+sentence)
+

f8c55305323de410c7d214af17fb51885986cee7
brian7685 <mac@macdeMacBook-Air-3.local>
2015-05-04 19:28:48 +0800

    noTagTest

---
+++ phrase1/src/noTagTest.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+nGram = {}
+nGramHashKeyList = []
+line = open("../input/p1.test.txt",'r')
+rightNgram = open("../tmp/noTagTest.txt",'w')
+
+for i in xrange(200):#5924 2962
+    data = line.readline()
+    token = data.split()
+    sentence = token[1:]
+    word = sentence[0]
+    wordS = word.decode('utf8')
+    rightNgram.write(str(word)+'\n')
+

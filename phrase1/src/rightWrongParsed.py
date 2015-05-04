e57da255023439b91a4b50f2db5f5b05e668821b
KevinLJW <evin92@gmail.com>
2015-05-04 20:03:03 +0800

    rwp fixed

--- phrase1/src/rightWrongParsed.py
+++ phrase1/src/rightWrongParsed.py
@@ -8,7 +8,9 @@ line = open("input/240365_p1.train.txt",'r')
 parsed = open("tmp/segResult.txt",'r')
 rightNgram = open("tmp/rightParsed.txt",'w')
 worngNgram = open("tmp/wrongParsed.txt",'w')
- 
+mixNgram = open("tmp/mixParsed.txt",'w')
+
+
 
 for i in xrange(5924):#5924 2962
     data = line.readline()

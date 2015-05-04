b1bc73e0d52ce6f6756b152d626a383bd7611324
brian7685 <mac@macdeMacBook-Air-3.local>
2015-05-04 19:52:58 +0800

    rightWrongParsed

--- phrase1/src/rightWrongParsed.py
+++ phrase1/src/rightWrongParsed.py
@@ -6,15 +6,16 @@ nGramHashKeyList = []
 line = open("../input/240365_p1.train.txt",'r')
 parsed = open("../tmp/segResult.txt",'r')
 rightNgram = open("../tmp/rightParsed.txt",'w')
-worngNgram = open("../tmp/wrongParsed.txt",'w')
+wrongNgram = open("../tmp/wrongParsed.txt",'w')
+mixNgram = open("../tmp/mixParsed.txt",'w')
 
 for i in xrange(5924):#5924 2962
     data = line.readline()
     sentence = parsed.readline()
     token = data.split()
-
+    mixNgram.write(str(token[0])+'\t'+str(token[1])+'\t'+sentence)
     if int(token[1]) is 0:
         rightNgram.write(str(token[0])+'\t'+str(token[1])+'\t'+sentence)
     else:
-        worngNgram.write(str(token[0])+'\t'+str(token[1])+'\t'+sentence)
+        wrongNgram.write(str(token[0])+'\t'+str(token[1])+'\t'+sentence)
 

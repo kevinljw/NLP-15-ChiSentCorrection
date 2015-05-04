2b8b8339b9eeb07e96d31ccd71ef1ae74edd70d7
brian7685 <mac@macdeMacBook-Air-3.local>
2015-05-04 19:27:24 +0800

    noTag

---
+++ phrase1/src/noTag.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+nGram = {}
+nGramHashKeyList = []
+line = open("../input/240365_p1.train.txt",'r')
+rightNgram = open("../tmp/noTag.txt",'w')
+
+for i in xrange(5924):#5924 2962
+    data = line.readline()
+    token = data.split()
+    sentence = token[2:]
+    word = sentence[0]
+    wordS = word.decode('utf8')
+    rightNgram.write(str(word)+'\n')
+
